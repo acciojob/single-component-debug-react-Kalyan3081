91fa554e4c708824cce0b764b82d18b3d7746c71
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-11-21 15:47:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 // Convert App component to a functional component
 const App = () => {
     return (
-        <div>
+        <div id="main">
             <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
         </div>
     );
